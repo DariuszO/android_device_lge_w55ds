312c4b50ecc354836aeada17e37c6f60e2aa45da
vm03 <vm03@yandex.ru>
2015-05-21 21:38:03 +0300

    initial w55ds

---
+++ BoardConfig.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+-include device/lge/msm8610-common/BoardConfigCommon.mk
+
+TARGET_KERNEL_CONFIG := w55ds_global_com_defconfig
+BOARD_CUSTOM_BOOTIMG_MK := device/lge/w55ds/mkbootimg.mk
+
+DEVICE_RESOLUTION := 480x800
+
+#TWRP
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+
+-include vendor/lge/w5/BoardConfigVendor.mk
+
+TARGET_OTA_ASSERT_DEVICE := w5,w5ds,w55ds,gammaw
